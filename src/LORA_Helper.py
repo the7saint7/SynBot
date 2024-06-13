7dfc21823f5f55dd3a5111dc88952bebd1efcd9a
ROG\Work <SirWanksALot>
2024-06-12 23:36:23 -0400

    Using new THICK tag

--- src/LORA_Helper.py
+++ src/LORA_Helper.py
@@ -38,6 +38,10 @@ LORA_List = {
     "DOWN-BLOUSE": "<lora:EDB:1> downblouse, extended downblouse, leaning_forward",
     "HEADLESS": "<lora:DisembodiedHeadV2:1.0>, DisembodiedHead, holding head, headless",
     "GENIE": "<lora:genie3:.85>, genie, genie_tail, smoke_tail, smoke, magic_lamp, glowing, smoke_tail, smoke_hair, ponytail, magic, pointy_ears, bracer, belt, gold_belt, gold_bracer, floating",
+    "THICC-1": "<lora:thicc_v2.3:1>",
+    "THICC-0.5": "<lora:thicc_v2.3:0.5>",
+    "THICC-1.5": "<lora:thicc_v2.3:1.5>",
+    "THICC-0.75": "<lora:thicc_v2.3:0.75>",
 
      "OUTFIT_KAT_CASUAL": "<lora:stOutfits:.6>, stOutfits, katrina_casual, shirt, (((yellow_shirt))), cleavage, collarbone, midriff, navel,  blue_shorts, brown_belt, blue bra, taut shorts, bow, stKatrina, <lora:stKatrina:.4>",
      "OUTFIT_KAT_MAID": " <lora:stOutfits:.6>, stOutfits, katrina_maid, maid, white apron, dress, thighhighs, blue_dress,  <lora:stKatrina2:.4> , stKatrina, white collar, collared dress, black thighhighs,  short sleeves",
